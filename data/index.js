64097350d45ab5fcc363d46ddf15d6836129a9f5
vagabond1215 <vagabond1215@gmail.com>
2025-07-19 12:55:05 -0400

    Merge branch 'codex/add-local-save/load-function-to-character-page'

--- data/index.js
+++ data/index.js
@@ -1,4 +1,4 @@
 export { jobs, jobNames, baseJobNames } from './jobs.js';
 export { races, raceNames } from './races.js';
-export { characters, createNewCharacter, calculateCharacterStats, updateDerivedStats, saveCharacters, loadCharacters } from './characters.js';
+export { characters, createNewCharacter, calculateCharacterStats, updateDerivedStats, saveCharacters, loadCharacters, clearSavedCharacters } from './characters.js';
 export { proficiencyScale, getScale } from './scales.js';
