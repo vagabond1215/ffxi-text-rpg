08bfe08fa0b70f4da81dbbb4eaf551bbcd811200
vagabond1215 <vagabond1215@gmail.com>
2025-08-04 12:15:19 -0400

    Add weapon and magic proficiency system

---
+++ data/proficiencies.js
@@ -0,0 +1,32 @@
+export const weaponSkills = [
+  'Hand-to-Hand',
+  'Dagger',
+  'Sword',
+  'Great Sword',
+  'Axe',
+  'Great Axe',
+  'Scythe',
+  'Polearm',
+  'Katana',
+  'Great Katana',
+  'Club',
+  'Staff',
+  'Archery',
+  'Marksmanship',
+  'Throwing'
+];
+
+export const magicSkills = [
+  { name: 'Elemental Magic', magicType: 'Black Magic', subType: 'Elemental' },
+  { name: 'Dark Magic', magicType: 'Black Magic', subType: 'Dark' },
+  { name: 'Enfeebling Magic', magicType: 'Black Magic', subType: 'Enfeebling' },
+  { name: 'Enhancing Magic', magicType: 'White Magic', subType: 'Enhancing' },
+  { name: 'Healing Magic', magicType: 'White Magic', subType: 'Healing' },
+  { name: 'Divine Magic', magicType: 'White Magic', subType: 'Divine' },
+  { name: 'Summoning Magic', magicType: 'Summoning Magic', subType: 'Summoning' },
+  { name: 'Ninjutsu', magicType: 'Ninjutsu', subType: 'Ninjutsu' },
+  { name: 'Singing', magicType: 'Bard', subType: 'Singing' },
+  { name: 'Stringed Instrument', magicType: 'Bard', subType: 'Instrument' },
+  { name: 'Wind Instrument', magicType: 'Bard', subType: 'Instrument' },
+  { name: 'Blue Magic', magicType: 'Blue Magic', subType: 'Blue Magic' }
+];
