678ca21fb3bb772171939909ab40474786cbbb52
vagabond1215 <vagabond1215@gmail.com>
2025-08-03 21:38:41 -0400

    Fix duplicate spawn and targeting for linked monsters

--- js/ui.js
+++ js/ui.js
@@ -2456,7 +2456,9 @@ function createActionPanel(root, loc) {
                 m.listIndex = nearbyMonsters.length;
                 m.hp = m.hp ?? parseLevel(m.level) * 20;
                 nearbyMonsters.push(m);
-                if (activeCharacter) activeCharacter.monsters.push(m);
+                if (activeCharacter && activeCharacter.monsters !== nearbyMonsters) {
+                    activeCharacter.monsters.push(m);
+                }
             }
         });
         renderCombatScreen(root.parentElement, group);
