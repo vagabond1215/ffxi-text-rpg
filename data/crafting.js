7f4f366885f43ef7dd38d45e55ebfc9f468d93ad
vagabond1215 <vagabond1215@gmail.com>
2025-08-04 23:58:20 -0400

    Add crafting data

---
+++ data/crafting.js
@@ -0,0 +1,49 @@
+export const crafts = [
+  {
+    name: 'Alchemy',
+    description: 'Creates potions and chemical concoctions.',
+    guild: 'Bastok Markets'
+  },
+  {
+    name: 'Bonecraft',
+    description: 'Shapes bone and shell into equipment.',
+    guild: 'Windurst Woods'
+  },
+  {
+    name: 'Clothcraft',
+    description: 'Weaves fabrics and tailors clothing.',
+    guild: 'Windurst Woods'
+  },
+  {
+    name: 'Cooking',
+    description: 'Prepares meals and beverages.',
+    guild: 'Windurst Waters'
+  },
+  {
+    name: 'Fishing',
+    description: 'Catches fish with rods and bait.',
+    guild: 'Port Windurst'
+  },
+  {
+    name: 'Goldsmithing',
+    description: 'Works precious metals and gems.',
+    guild: 'Bastok Mines'
+  },
+  {
+    name: 'Leathercraft',
+    description: 'Tans hides and crafts leather gear.',
+    guild: 'Southern San d\'Oria'
+  },
+  {
+    name: 'Smithing',
+    description: 'Forges metal weapons and armor.',
+    guild: 'Bastok Markets'
+  },
+  {
+    name: 'Woodworking',
+    description: 'Shapes lumber into tools and weapons.',
+    guild: 'Northern San d\'Oria'
+  }
+];
+
+export const craftNames = crafts.map(c => c.name);
