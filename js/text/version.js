60420bdc33c39e03c2e606a38c08bb953b243757
vagabond1215 <vagabond1215@gmail.com>
2026-05-21 14:45:26 -0400

    Track combat action system version

--- js/text/version.js
+++ js/text/version.js
@@ -1,15 +1,15 @@
 export const VERSION = Object.freeze({
-    app: '0.3.1',
+    app: '0.3.2',
     save: 2,
     data: 3,
     benchmark: 1,
-    codename: 'Nation Start Foundation',
+    codename: 'Combat Action Foundation',
     compatibility: 'no-backwards-compatibility',
     released: false,
 });
 
 export const SYSTEM_VERSIONS = Object.freeze({
-    commandShell: '0.3.1',
+    commandShell: '0.3.2',
     parser: '0.2.0',
     validation: '0.3.1',
     playerEntity: '0.3.1',
@@ -19,7 +19,8 @@ export const SYSTEM_VERSIONS = Object.freeze({
     enemyEntity: '0.1.0',
     statEngine: '0.1.0',
     statusEngine: '0.1.0',
-    battleEngine: '0.1.0',
+    battleEngine: '0.3.2',
+    combatActions: '0.3.2',
     liveTick: '0.2.0',
     maps: '0.3.1',
     zones: '0.3.1',
