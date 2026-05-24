226a97aeb75d9c25406f9f7ab38972c6d561df5a
vagabond1215 <vagabond1215@gmail.com>
2026-05-24 16:07:42 -0400

    Make server launcher forward to Start Server

--- server.cmd
+++ server.cmd
@@ -1,24 +1,3 @@
 @echo off
-setlocal
 cd /d "%~dp0"
-
-where node >nul 2>nul
-if errorlevel 1 (
-    echo Node.js 20 or newer is required to run the FFXI Text RPG local server.
-    echo Download Node.js from https://nodejs.org/
-    pause
-    exit /b 1
-)
-
-echo Starting FFXI Text RPG local server...
-echo.
-echo Open this URL in your browser:
-echo http://127.0.0.1:4173/
-echo.
-echo Press Ctrl+C in this window to stop the server.
-echo.
-npm run serve
-
-echo.
-echo Server stopped.
-pause
+call "%~dp0Start Server.cmd"
