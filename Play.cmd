d330f87bd7ab440630948b34d801227ca0f589de
vagabond1215 <vagabond1215@gmail.com>
2026-08-12 12:51:43 -0400

    Update launcher branding to Hearth and Horizon

--- Play.cmd
+++ Play.cmd
@@ -2,7 +2,7 @@
 setlocal
 
 set "GAME_URL=http://127.0.0.1:4173/"
-echo Opening FFXI Text RPG...
+echo Opening Hearth ^& Horizon...
 echo %GAME_URL%
 echo.
 echo If the page does not load, run "Start Server.cmd" first.
