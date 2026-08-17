44e953b04ad4c4631d1cb3e2d7e6c038cbd14c4d
vagabond1215 <vagabond1215@gmail.com>
2026-08-17 19:13:00 -0400

    Register hide bindings as portable construction material

--- js/text/data/productionItems.js
+++ js/text/data/productionItems.js
@@ -1,6 +1,6 @@
 import { ITEM_KINDS, normalizeItem } from './itemSchema.js';
 
-export const PRODUCTION_ITEM_CATALOG_VERSION = 3;
+export const PRODUCTION_ITEM_CATALOG_VERSION = 4;
 
 const PRODUCTION_ITEM_DEFINITIONS = Object.freeze({
     'item-redstone-copper-ingot': productionItem({ id: 'item-redstone-copper-ingot', name: 'Redstone Copper Ingot', kind: ITEM_KINDS.MATERIAL, tags: ['metal', 'copper', 'component', 'crafted'], valueGil: 28, sourceId: 'process-redstone-copper-ingot', action: 'process', sinks: ['craftIngredient', 'processInput', 'construction', 'trade'] }),
@@ -8,7 +8,7 @@ const PRODUCTION_ITEM_DEFINITIONS = Object.freeze({
     'item-silverfin-sweetroot-stew': productionItem({ id: 'item-silverfin-sweetroot-stew', name: 'Silverfin Sweetroot Stew', kind: ITEM_KINDS.CONSUMABLE, tags: ['food', 'meal', 'cooked', 'starfen'], valueGil: 24, sourceId: 'cook-silverfin-sweetroot-stew', action: 'craft', sinks: ['consume', 'trade'] }),
     'item-copper-scrap': productionItem({ id: 'item-copper-scrap', name: 'Copper Scrap', kind: ITEM_KINDS.MATERIAL, tags: ['metal', 'copper', 'scrap', 'salvage'], valueGil: 8, sourceId: 'salvage-copper-trail-clasp', action: 'salvage', sinks: ['processInput', 'trade'] }),
     'item-elderwood-resin-board': productionItem({ id: 'item-elderwood-resin-board', name: 'Resin-Sealed Hardwood Board', kind: ITEM_KINDS.MATERIAL, tags: ['wood', 'component', 'sealed', 'elderwood'], valueGil: 38, sourceId: 'craft-elderwood-resin-board', action: 'craft', sinks: ['craftIngredient', 'construction', 'repair', 'trade'] }),
-    'item-elderwood-hide-binding': productionItem({ id: 'item-elderwood-hide-binding', name: 'Resin-Cured Hide Binding', kind: ITEM_KINDS.MATERIAL, tags: ['hide', 'binding', 'component', 'elderwood'], valueGil: 34, sourceId: 'craft-elderwood-hide-binding', action: 'craft', sinks: ['craftIngredient', 'repair', 'trade'] }),
+    'item-elderwood-hide-binding': productionItem({ id: 'item-elderwood-hide-binding', name: 'Resin-Cured Hide Binding', kind: ITEM_KINDS.MATERIAL, tags: ['hide', 'binding', 'component', 'elderwood'], valueGil: 34, sourceId: 'craft-elderwood-hide-binding', action: 'craft', sinks: ['craftIngredient', 'construction', 'repair', 'trade'] }),
     'item-redstone-iron-bloom': productionItem({ id: 'item-redstone-iron-bloom', name: 'Redstone Iron Bloom', kind: ITEM_KINDS.MATERIAL, tags: ['metal', 'iron', 'component', 'redstone'], valueGil: 36, sourceId: 'process-redstone-iron-bloom', action: 'process', sinks: ['craftIngredient', 'processInput', 'repair', 'trade'] }),
     'item-starfen-bluekelp-broth': productionItem({ id: 'item-starfen-bluekelp-broth', name: 'Bluekelp Silverfin Broth', kind: ITEM_KINDS.CONSUMABLE, tags: ['food', 'meal', 'cooked', 'starfen'], valueGil: 27, sourceId: 'cook-starfen-bluekelp-broth', action: 'craft', sinks: ['consume', 'trade'] }),
 });
