a2973dcda20e439861db09ffb8226f4544b42073
vagabond1215 <vagabond1215@gmail.com>
2026-08-28 16:28:13 -0400

    Connect Great Mere resources to canonical registry

--- js/text/data/resourceItemRegistry.js
+++ js/text/data/resourceItemRegistry.js
@@ -1,18 +1,19 @@
 import { validateItemConsumption } from './itemSchema.js';
 import { getPlace } from './places.js';
 import { validateItemResourceMetadata } from './resourceProvenance.js';
+import { getGreatMereResourceItem, listGreatMereResourceItems } from './greatMereResourceItems.js';
 import { getHuntingResourceItem, listHuntingResourceItems } from './huntingResourceItems.js';
 import { getResourceItem, listResourceItems } from './resourceItems.js';
 import { getRegionalResourceItem, listRegionalResourceItems } from './regionalResourceItems.js';
 
-export const RESOURCE_ITEM_REGISTRY_VERSION = 2;
+export const RESOURCE_ITEM_REGISTRY_VERSION = 3;
 
 export function getCanonicalResourceItem(itemId) {
-    return getResourceItem(itemId) ?? getRegionalResourceItem(itemId) ?? getHuntingResourceItem(itemId);
+    return getResourceItem(itemId) ?? getRegionalResourceItem(itemId) ?? getHuntingResourceItem(itemId) ?? getGreatMereResourceItem(itemId);
 }
 
 export function listCanonicalResourceItems() {
-    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems()];
+    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems(), ...listGreatMereResourceItems()];
     const ids = new Set();
     return items.filter((item) => {
         if (ids.has(item.id)) return false;
@@ -24,7 +25,7 @@ export function listCanonicalResourceItems() {
 export function validateResourceItemRegistry() {
     const issues = [];
     const ids = new Set();
-    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems()];
+    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems(), ...listGreatMereResourceItems()];
     for (const item of items) {
         if (ids.has(item.id)) issues.push(`Duplicate canonical resource item ${item.id}.`);
         ids.add(item.id);
