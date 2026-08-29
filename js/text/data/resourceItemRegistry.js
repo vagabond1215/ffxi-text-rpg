03aa340f63ca2d6dd61325f41c5f5791320e2645
vagabond1215 <vagabond1215@gmail.com>
2026-08-29 18:34:34 -0400

    Register Emberwash resources

--- js/text/data/resourceItemRegistry.js
+++ js/text/data/resourceItemRegistry.js
@@ -1,6 +1,7 @@
 import { validateItemConsumption } from './itemSchema.js';
 import { getPlace } from './places.js';
 import { validateItemResourceMetadata } from './resourceProvenance.js';
+import { getEmberwashResourceItem, listEmberwashResourceItems } from './emberwashResourceItems.js';
 import { getGreatMereResourceItem, listGreatMereResourceItems } from './greatMereResourceItems.js';
 import { getGloamwoodResourceItem, listGloamwoodResourceItems } from './gloamwoodResourceItems.js';
 import { getHeadwaterResourceItem, listHeadwaterResourceItems } from './headwaterResourceItems.js';
@@ -11,14 +12,14 @@ import { getStarfenDeltaResourceItem, listStarfenDeltaResourceItems } from './st
 import { getResourceItem, listResourceItems } from './resourceItems.js';
 import { getRegionalResourceItem, listRegionalResourceItems } from './regionalResourceItems.js';
 
-export const RESOURCE_ITEM_REGISTRY_VERSION = 9;
+export const RESOURCE_ITEM_REGISTRY_VERSION = 10;
 
 export function getCanonicalResourceItem(itemId) {
-    return getResourceItem(itemId) ?? getRegionalResourceItem(itemId) ?? getHuntingResourceItem(itemId) ?? getGreatMereResourceItem(itemId) ?? getIronspineResourceItem(itemId) ?? getHeadwaterResourceItem(itemId) ?? getStarfenDeltaResourceItem(itemId) ?? getGloamwoodResourceItem(itemId) ?? getMaterialFoundationResourceItem(itemId);
+    return getResourceItem(itemId) ?? getRegionalResourceItem(itemId) ?? getHuntingResourceItem(itemId) ?? getEmberwashResourceItem(itemId) ?? getGreatMereResourceItem(itemId) ?? getIronspineResourceItem(itemId) ?? getHeadwaterResourceItem(itemId) ?? getStarfenDeltaResourceItem(itemId) ?? getGloamwoodResourceItem(itemId) ?? getMaterialFoundationResourceItem(itemId);
 }
 
 export function listCanonicalResourceItems() {
-    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems(), ...listGreatMereResourceItems(), ...listIronspineResourceItems(), ...listHeadwaterResourceItems(), ...listStarfenDeltaResourceItems(), ...listGloamwoodResourceItems(), ...listMaterialFoundationResourceItems()];
+    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems(), ...listEmberwashResourceItems(), ...listGreatMereResourceItems(), ...listIronspineResourceItems(), ...listHeadwaterResourceItems(), ...listStarfenDeltaResourceItems(), ...listGloamwoodResourceItems(), ...listMaterialFoundationResourceItems()];
     const ids = new Set();
     return items.filter((item) => {
         if (ids.has(item.id)) return false;
@@ -30,7 +31,7 @@ export function listCanonicalResourceItems() {
 export function validateResourceItemRegistry() {
     const issues = [];
     const ids = new Set();
-    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems(), ...listGreatMereResourceItems(), ...listIronspineResourceItems(), ...listHeadwaterResourceItems(), ...listStarfenDeltaResourceItems(), ...listGloamwoodResourceItems(), ...listMaterialFoundationResourceItems()];
+    const items = [...listResourceItems(), ...listRegionalResourceItems(), ...listHuntingResourceItems(), ...listEmberwashResourceItems(), ...listGreatMereResourceItems(), ...listIronspineResourceItems(), ...listHeadwaterResourceItems(), ...listStarfenDeltaResourceItems(), ...listGloamwoodResourceItems(), ...listMaterialFoundationResourceItems()];
     for (const item of items) {
         if (ids.has(item.id)) issues.push(`Duplicate canonical resource item ${item.id}.`);
         ids.add(item.id);
